8c69aaa50bdb328d7c50b621c81fdf0cf4d95cbe
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-08 17:32:34 +0200

    events_ref_state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  let a = 5;
+  let textInput = React.createRef();
+  let textOut = React.createRef();
+  const [output, setOutput] = useState('Hello');
+
+  function f1(arg) {
+    console.log('f1 work!' + arg);
+  }
+  function f2() {
+    console.log('move!');
+  }
+
+  function showInput(event) {
+    console.log('input');
+    // console.log(this.value);  //!! Разобраться как работает
+    // console.log(event.target.value)
+    console.log(textInput.current.value)
+    textOut.current.innerHTML = textInput.current.value;
+    setOutput(textInput.current.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>События</h1>
+        <h2>Button</h2>
+        <button onClick={()=>f1(10)}>Push</button>
+      </section>
+      <section>
+        <h2>DBL Click + Mouse move</h2>
+        <div className="test" onDoubleClick={()=>f1(20)}></div>
+      </section>
+      <section>
+        <h1>Input</h1>
+        <input type="text" onInput={showInput} ref={textInput} defaultValue="Hi" placeholder="value..."/>
+        <div className="out" ref={textOut}></div>
+        <h3>{output}</h3>
+      </section>
     </div>
   );
 }
